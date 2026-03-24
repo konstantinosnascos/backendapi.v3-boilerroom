fcf10444a2e798fba93bd73df0a79af91807d9f7
Felix Liden <>
2026-03-24 09:42:43 +0100

    Added: OrderIntegrationTest

---
+++ boilerroom2/shop/src/test/java/com/example/shop/OrderIntegrationTest.java
@@ -0,0 +1,28 @@
+package com.example.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Map;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class OrderIntegrationTest
+{
+    @Autowired
+    org.springframework.boot.test.web.client.TestRestTemplate restTemplate;
+
+    @Test
+    void CreateOrderWithInvalidCustomer()
+    {
+        Map<String,Long> map = Map.of("customerId",999L);
+
+        ResponseEntity<String> response = restTemplate.postForEntity("/api/v2/orders", map, String.class);
+
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Customer not found");
+    }
+}
